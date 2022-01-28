b3df41354cac5ba8ecc342a52a1ddca1076dfeeb
luiz.leal <luiz.leal@jdsystem.com.br>
2022-01-28 19:00:44 -0300

    Mostrando outros deals

--- Pages/Home/DealFocused/index.js
+++ Pages/Home/DealFocused/index.js
@@ -1,28 +1,52 @@
 import React , { useEffect, useState} from 'react'
-import { View , Image , SafeAreaView ,Pressable, Text , StatusBar , ActivityIndicator , FlatList} from 'react-native';
+import { View , Image , SafeAreaView ,Pressable, Text , Linking, StatusBar ,useWindowDimensions, ActivityIndicator , FlatList} from 'react-native';
 import api from '../../../Services/api';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
-const createDealList = (deal) => {
-    return (
-        <View style={{margin:20}}>
-            <Text style={{color:"white"}}>{deal.item.price}</Text>
-        </View>
-    )
-}
+import { LinearGradient } from 'expo-linear-gradient';
+const url = "https://www.cheapshark.com/redirect?dealID=";
 
 export default function DealFocused({route,navigation}){
+    const window = useWindowDimensions();
     const gameID = route.params;
     const [loading,setLoading] = useState(false);
     const [response,setResp] = useState();
+    const [stores,setStores] = useState();
+
+    const createDealList = (deal) => {
+            var store = stores?.find( store => store.storeID == deal.item.storeID)
+        return (
+            <Pressable onPress={()=>Linking.openURL(url+deal.item.dealID)} style={{margin:20,flexDirection:'row',width:window.width - 50,alignItems:'center',justifyContent:'space-between'}}>
+                <View style={{flexDirection:'row',alignItems:'center'}}>
+                    <Image style={{resizeMode:'stretch',width:50,height:50,opacity:0.9}}source={{uri : "https://www.cheapshark.com/"+store?.images.logo }}></Image>
+                    <Text style={{color:"white" , marginLeft:20}}>{store?.storeName}</Text>
+                </View>
+
+                <View style={{alignItems:'center'}}>
+                    <Text style={{color:'grey',marginBottom:5,fontSize:10,textDecorationLine: 'line-through',textDecorationStyle: 'solid'}}>{deal.item.retailPrice}</Text>
+                    <Text style={{color:'rgba(5,147,132,1)',fontSize:17}}>{deal.item.price}</Text>
+                </View>
+ 
+            </Pressable>
+        )
+    }
     
+    useEffect(async () => {
+        await api
+          .get("https://www.cheapshark.com/api/1.0/stores")
+          .then((response) => {setStores(response.data) ;})
+          .catch((err) => {
+            console.error("ops! ocorreu um erro" + err);
+          });
+      }, [gameID]);
+
     const MemoizedList = React.memo(() => {
         return (
           <FlatList
           removeClippedSubviews = {true}
           initialNumToRender = {9}
           showsVerticalScrollIndicator={false}
-          maxToRenderPerBatch = {9}data={response?.deals}
+          maxToRenderPerBatch = {9}
+          data={response?.deals}
           renderItem={createDealList}
           keyExtractor={item => item.dealID}
           />
@@ -55,16 +79,37 @@ export default function DealFocused({route,navigation}){
     return ( 
         <SafeAreaView style={{flex:1,marginTop: StatusBar.currentHeight || 0, alignItems: 'center', justifyContent: 'center',backgroundColor:'#192428'}}> 
             {!loading && <LoadingView/>}
-                <View style={{width:'100%',height:253,backgroundColor:'red'}} >
+                <View style={{width:'100%',height:253}} >
                     <Pressable style={{position:'absolute',top:10,right:5,zIndex:20}} onPress={()=> navigation.goBack()}>
                         <Ionicons name={'close-circle-outline'} size={30} color={'black'} />
                     </Pressable>
-                    <Image style={{resizeMode:'stretch',width:400,height:253}}source={{uri : response?.info.thumb.replace("capsule_sm_120","capsule_616x353")}}></Image>
+                    <Image style={{resizeMode:'stretch',width:400,height:223,opacity:0.9}}source={{uri : response?.info.thumb.replace("capsule_sm_120","capsule_616x353")}}></Image>
+                </View>
+
+                <View style={{alignItems:'center',justifyContent:'center'}}>
+                    <Text style={{color:'white',fontSize:25,marginLeft:10}}>{response?.info.title}</Text>
+                    
+                    <LinearGradient style={{marginVertical:15,borderRadius:8,backgroundColor:'white',width:150,height:50,alignItems:'center',justifyContent:'center'}} colors={['#0784b5', 'rgba(5,147,132,1)', 'rgba(5,147,132,1)','rgba(1,33,91,1)']}> 
+                        <Pressable style={{alignItems:'center',justifyContent:'center',flexDirection:'row'}} onPress={()=>Linking.openURL(url+response?.dealID)}>
+                            <Ionicons name={'cart-outline'} size={30} color={'black'} />
+                            <Text style={{marginLeft:10}}>Get Offer</Text>
+                        </Pressable>
+                    </LinearGradient>
                 </View>
 
                 <View style={{flex:2}}>
-                    <Text>{response?.info.title}</Text>
-                    <Text>Deals</Text>
+                    {response?.deals !== null ? 
+                    (
+                    <>
+                        <Text style={{color:'white',fontSize:25,marginLeft:10}}>Deals</Text>
+                        <MemoizedList/>
+                    </>
+                    )
+                    : 
+                    <>
+                    <Text style={{color:'white'}}>No more deals Found</Text>
+                    </>
+                    }
                 </View>
      </SafeAreaView>
     )
